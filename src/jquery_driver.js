43db003efdd19199adbe5b6ad3cd9c84d48e1e4f
charles milam <charles.milam@gmail.com>
2015-01-03 08:58:47 -0600

    Modify, clean up of code.

--- src/jquery_driver.js
+++ src/jquery_driver.js
@@ -11,18 +11,61 @@ var charToNum = {
 }
 
 var displayBoard = function() {
+  var backColor = "red";
+  var row, square;
   for (var i = 0; i < board.length; i++) {
     for (var j = 0; j < board[i].length; j++) {
-      //console.log(board[i][j]);
       switch (board[i][j]) {
         case "empty":
-          console.log("empty");
+          row = document.getElementsByClassName("row-" + numToChar[i]); 
+          square = row[0].getElementsByClassName("col-" + j.toString()); 
+          
+          square[0].classList.remove("white");
+          square[0].classList.remove("red");
+          square[0].classList.remove("piece");
+          square[0].classList.add("empty");
+          
+          // if (backColor === "red") {
+          //   backColor = "black";
+          // }
+          // else {
+          //   backColor = "red";
+          // }
+          // square[0].setAttribute("background-color", backColor);
           break;
         case "white":
-          console.log("white");
+          row = document.getElementsByClassName("row-" + numToChar[i]); 
+          square = row[0].getElementsByClassName("col-" + j.toString()); 
+          
+          square[0].classList.remove("red");
+          square[0].classList.remove("empty");
+          square[0].classList.add("white");
+          square[0].classList.add("piece");
+          
+          // if (backColor === "red") {
+          //   backColor = "black";
+          // }
+          // else {
+          //   backColor = "red";
+          // }
+          // square[0].setAttribute("background-color", backColor);
           break;
         case "red  ":
-          console.log("red");
+          row = document.getElementsByClassName("row-" + numToChar[i]); 
+          square = row[0].getElementsByClassName("col-" + j.toString()); 
+          
+          square[0].classList.remove("white");
+          square[0].classList.remove("empty");
+          square[0].classList.add("red");
+          square[0].classList.add("piece");
+          
+          // if (backColor === "red") {
+          //   backColor = "black";
+          // }
+          // else {
+          //   backColor = "red";
+          // }
+          // square[0].setAttribute("background-color", backColor);
           break;
       }
     };
